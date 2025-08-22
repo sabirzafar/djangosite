608a85d3708527befa1d7d39d62c655ce36c331f
sabirzafar <sabir_zafar@yahoo.com>
2025-08-22 22:37:45 +0400

    npm path url updated

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = ['127.0.0.1']
-NPM_BIN_PATH = ""
+NPM_BIN_PATH = "/home/sabir/.nvm/versions/node/v20.12.2/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
